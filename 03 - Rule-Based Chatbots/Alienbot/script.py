0958c00975b9a5bf30837c7b42e3e54354ba3971
Dyrits <dyrits@outlook.com>
2020-11-18 20:29:06 +0300

    Minor re-factoring.

--- 03 - Rule-Based Chatbots/Alienbot/script.py
+++ 03 - Rule-Based Chatbots/Alienbot/script.py
@@ -1,4 +1,3 @@
-# importing regex and random libraries
 import re
 import random
 
@@ -14,65 +13,65 @@ class AlienBot:
                         "What planets have you visited?",
                         "What technology do you have on this planet?")
 
-    def __init__(self):
+    def __init__(self, name="Etcaetera"):
+        self.username = None
+        self.name = name
         self.intents = {
             r'.*your planet.*': self.describe_planet_intent,
             r'.*why are.*': self.answer_why_intent,
-            r'.*cube.*(?P<number>\d+).*': self.cubed_intent
+            r'.*cube.*?(\d+).*': self.cubed_intent
         }
 
     def greet(self):
-        self.name = input("Hello there! What's your name? \n")
-        will_help = input(
-            f"Hi {self.name}, I'm Etcetera. I'm not from this planet. Will you help me learn about your planet? \n")
-        if will_help.lower() in self.negative_responses:
-            return self.exit()
-        self.chat()
+        self.username = input("Hello there! What's your name? \n")
+        will_help = input(f"Hi {self.username}, I'm {self.name}. I'm not from this planet. Will you help me learn about your planet? \n")
+        return self.exit() if will_help.lower() in self.negative_responses else self.chat()
 
     def exit(self):
-        print(f"Okay {self.name}, have a nice Earth day! \n")
+        print(f"Okay {self.username}, have a nice Earth day! \n")
 
     def make_exit(self, reply):
         for word in self.exit_commands:
             if reply in word:
-                return self.exit()
+                return self.exit() or True
 
     def chat(self):
-        while not self.make_exit(reply := input(random.choice(self.random_questions)).lower()):
+        while not self.make_exit(reply := input(random.choice(self.random_questions) + "\n").lower()):
             self.match_reply(reply)
 
     def match_reply(self, reply):
         for regex_pattern, intent in self.intents.items():
-            if found_match := re.match(regex_pattern, reply, re.IGNORECASE):
-                print(intent(found_match.group(1)) if found_match.groups() else intent())
-            else:
-                print(self.no_match_intent())
+            if found_match := re.match(regex_pattern, reply):
+                parameters = found_match.groups()
+                return intent(*parameters)
+        return self.no_match_intent()
 
     def describe_planet_intent(self):
-        responses = ("My planet is a utopia of diverse organisms and species.",
+        responses = (f"My planet, {self.username}, is an utopia of diverse organisms and species.",
                      "I am from Opidipus, the capital of the Wayward Galaxies. ")
-        return random.choice(responses)
+        print(random.choice(responses))
 
     def answer_why_intent(self):
         responses = ("I come in peace.",
-                     "I am here to collect data on your planet and its inhabitants.",
+                     f"I am here to collect data on your planet and its inhabitants, including you {self.username}.",
                      "I heard the coffee is good.")
-        return random.choice(responses)
+        print(random.choice(responses))
 
     def cubed_intent(self, number):
-        return f"The cube of {number} is {int(number) ** 3}. Isn't that cool?"
+        print(f"The cube of {number} is {int(number) ** 3}. Isn't that cool, {self.username}?")
 
     def no_match_intent(self):
-        responses = ("Please tell me more.",
+        responses = (f"Please {self.username} tell me more.",
                      "Tell me more!",
                      "Why do you say that?",
-                     "I see. Can you elaborate?",
+                     f"I see, {self.username}. Can you elaborate?",
                      "Interesting. Can you tell me more?",
                      "I see. How do you think?",
-                     "Why?",
+                     f"Why? {self.username}, why?",
                      "How do you think I feel when you say that?"
                      )
-        return random.choice(responses)
+        print(random.choice(responses))
 
 
-AlienBot().greet()
+if __name__ == '__main__':
+    AlienBot().greet()
